259e5aa8d9a954ea486cc90a6d1cf4e17f20ba9b
aliuzunoglan <AliTayyip.Uzunoglan@turkiyefinans.com.tr>
2023-02-09 02:45:19 +0300

    IService class generated

---
+++ SRooms.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SRooms.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : class
+	{
+        // productRepository.where(x=>x.id>5).OrderBy.ToList() veya Tolistasync methodu çağrılabilir;
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        Task<T> GetByIdAsync(int id);
+
+		// productRepository.where(x=>x.id>5).OrderBy.ToListAsync();
+		IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+		Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+		//update add methoduna göre daha kısa bi işlem olduğu için async olmasına gerek yoktur.
+		void Update(T entity);
+
+		void Remove(T entity);
+
+		void RemoveRange(IEnumerable<T> entitites);
+
+    }
+}
+
