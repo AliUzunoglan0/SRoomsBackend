d14f4f23042426f311d4170734356b512ef1752e
aliuzunoglan <AliTayyip.Uzunoglan@turkiyefinans.com.tr>
2023-02-09 02:15:40 +0300

    initial commit

---
+++ SRooms.Core/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SRooms.Core
+{
+	public abstract class BaseEntity
+	{
+		public int Id { get; set; }
+
+		public DateTime CreateTime { get; set; }
+
+		public DateTime UpdateTime { get; set; }
+	}
+}
+
---
+++ SRooms.Core/Category.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SRooms.Core
+{
+	public class Category:BaseEntity
+	{
+		public string Name { get; set; }
+	}
+}
+
