b7d05bc3b0f45a5ef6fb64cd5f1237f8bbecd9a7
aliuzunoglan <AliTayyip.Uzunoglan@turkiyefinans.com.tr>
2023-02-26 23:27:32 +0300

    custom response dto created

---
+++ SRooms.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace SRooms.Core.DTOs
+{
+	public class CustomResponseDto<T>
+	{
+
+		public T Data { get; set; }
+
+		public List<String> Errors { get; set; }
+
+		[JsonIgnore]
+		public int StatusCode { get; set; }
+
+
+	}
+}
+
